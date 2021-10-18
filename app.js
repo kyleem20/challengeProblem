97a80364732df31316978a735bc69825cd2a7ad5
Kylee Marlin <kylee.j.marlin@gmail.com>
2021-10-17 23:01:43 -0600

    updates

--- app.js
+++ app.js
@@ -1,20 +1,21 @@
 
 let bubbles = 0;
-
+let bubbleIncrease = 1
+let cost = 3
+let costMultiplier = 2
+let collecting = 3
 
 let clickUpgrades = {
   smallBag: {
     name: 'Small Bag',
     count: 0,
     price: 3,
-    increase: 2,
     multiplier: 1
   },
   largeBag: {
     name: 'Large Bag',
     count: 0,
     price: 5,
-    increase: 10,
     multiplier: 1
   }
 }
@@ -23,7 +24,7 @@ let automaticUpgrades = {
   fish: {
     name: 'Single Fish',
     count: 0,
-    price: 15,
+    price: 150,
     increase: 4,
     multiplier: 2
   },
@@ -37,7 +38,7 @@ let automaticUpgrades = {
   mermaid: {
     name: 'Mermaid',
     count: 0,
-    price: 100,
+    price: 1000,
     increase: 40,
     multiplier: 15
   }
@@ -45,7 +46,7 @@ let automaticUpgrades = {
 
 
 function mine() {
-  bubbles++
+  bubbles += bubbleIncrease
   // alert(bubbles)
   console.log(bubbles)
 
@@ -54,32 +55,185 @@ function mine() {
 
 function update() {
   document.getElementById('totalBubbles').innerText = `Total Bubbles: ${bubbles}`
+
 }
 
-function draw() {
-  let upgradedItem = ''
-  for (let key in clickUpgrades) {
-    let clickUpgradedto = clickUpgrades[key]
-  }
+function update2() {
+  document.getElementById('totalBubbleIncrease').innerText = `Total Additional Bubbles Collected: ${bubbleIncrease}`
 }
-
-
 
 function buySmallBag() {
+  let totalSmallBags = clickUpgrades['smallBag']
 
-  if (bubbles >= 3) {
+  let increaseofSmallBags = (totalSmallBags.count + 1) * totalSmallBags.price
+  console.log('increase', increaseofSmallBags)
+  if (bubbles >= increaseofSmallBags) {
 
-    bubbles -= 3
-    let totalSmallBags = clickUpgrades['smallBag']
-    console.log(totalSmallBags)
     totalSmallBags.count++
+    let newIncreaseSmallBag = totalSmallBags.count + 1
+    bubbles -= increaseofSmallBags
+    increaseofSmallBags = (increaseofSmallBags * costMultiplier)
+
+
     document.getElementById('totalSmallBags').innerText = `Total Small Bags: ${totalSmallBags.count}`
+    document.getElementById('buySmallBags').innerText = `Small Bag: ${increaseofSmallBags} Bubbles (+${newIncreaseSmallBag + 2})`
+    bubbleIncrease += newIncreaseSmallBag
+    update()
+  } else {
+
+  }
+  update2()
+}
+
+function buyLargeBag() {
+  let totalLargeBags = clickUpgrades['largeBag']
+
+  let increaseofLargeBags = ((totalLargeBags.count + 1) * totalLargeBags.price)
+  console.log('increase', increaseofLargeBags)
+  if (bubbles >= increaseofLargeBags) {
+
+    totalLargeBags.count++
+    let newIncreaseLargeBag = totalLargeBags.count + 2
+    bubbles -= increaseofLargeBags
+    increaseofLargeBags = (increaseofLargeBags * costMultiplier)
+    document.getElementById('totalLargeBags').innerText = `Total Large Bags: ${totalLargeBags.count}`
+    document.getElementById('buyLargeBags').innerText = `Large Bag: ${increaseofLargeBags} Bubbles (+${newIncreaseLargeBag + 3})`
+    bubbleIncrease += newIncreaseLargeBag
+    update()
+  } else {
+
+  }
+  update2()
+}
+
+
+function buyFish() {
+  let totalFish = automaticUpgrades['fish']
+  let increaseofFish = ((totalFish.count + 1) * totalFish.price)
+  console.log('fish increase', increaseofFish)
+
+  if (bubbles >= increaseofFish) {
+
+    totalFish.count++
+    bubbles -= increaseofFish
+    increaseofFish = (increaseofFish * costMultiplier)
+
+    bubbleIncrease = increaseofFish + totalFish.multiplier
+    console.log('count', totalFish.count)
+    console.log('bubble increase', bubbleIncrease)
+
+    document.getElementById('totalFish').innerText = `Total Fish: ${totalFish.count}`
+    document.getElementById('buyFish').innerText = `Fish: ${increaseofFish} Bubbles`
+    collecting = 3
+    update()
     console.log(bubbles)
   } else {
 
   }
+  let interval = setInterval(3000)
+  collecting--
+  if (automaticUpgrades['fish'].count > 0) {
+    if (collecting < 0) {
+      bubbles += automaticUpgrades['fish'].increase
+    } else {
+
+    }
+  }
 }
 
-function buyItem() {
-  let purchaseItem = 
+function buyMermaid() {
+  let totalMermaid = automaticUpgrades['Mermaid']
+  let increaseofMermaid = ((totalMermaid.count + 1) * totalMermaid.price)
+  console.log('Mermaid increase', increaseofMermaid)
+
+  if (bubbles >= increaseofMermaid) {
+
+    totalMermaid.count++
+    bubbles -= increaseofMermaid
+    increaseofMermaid = (increaseofMermaid * costMultiplier)
+
+    bubbleIncrease = increaseofMermaid + totalMermaid.multiplier
+    console.log('count', totalMermaid.count)
+    console.log('bubble increase', bubbleIncrease)
+
+    document.getElementById('totalMermaid').innerText = `Total Mermaid: ${totalMermaid.count}`
+    document.getElementById('buyMermaid').innerText = `Mermaid: ${increaseofMermaid} Bubbles`
+    collecting = 3
+    update()
+    console.log(bubbles)
+  } else {
+
+  }
+  let interval = setInterval(3000)
+  collecting--
+  if (automaticUpgrades['Mermaid'].count > 0) {
+    if (collecting < 0) {
+      bubbles += automaticUpgrades['Mermaid'].increase
+    } else {
+
+    }
+  }
 }
+
+// function collection() {
+
+// }
+// function collectingTimer() {
+//   collecting--
+//   if (automaticUpgrades['fish'].count > 0) {
+//     if (collecting < 0) {
+//       bubbles += automaticUpgrades['fish'].increase
+//     } else {
+
+//     }
+//   }
+//   collection()
+//   update()
+// }
+
+// function updateDraw() {
+//   document.getElementById('upgradedItem').innerText = `Total Small Bags: ${upgradedItem.count}`
+//   document.getElementById('buySmallBags').innerText = `Small Bag: ${increaseofItem} Bubbles`
+
+// }
+
+function draw() {
+
+  // for (let key in clickUpgrades) {
+  //   let upgradedTo = clickUpgrades[key];
+  //   if (upgradedTo.name) {
+  //     let upgradedItem = upgradedTo.name
+  //   }
+
+  // }
+}
+
+// function purchasedItem() {
+//   for (let key in clickUpgrades) {
+//     var upgradedTo = clickUpgrades[key]
+//   }
+//   let upgradedItem = upgradedTo[key]
+//   let increaseofItem = ((upgradedItem.count + 1) * upgradedItem.price)
+//   console.log('increase', increaseofItem)
+//   if (bubbles >= increaseofItem) {
+
+//     upgradedItem.count++
+//     bubbles -= increaseofItem
+//     increaseofItem = (increaseofItem * costMultiplier)
+
+//     bubbleIncrease = increaseofItem + upgradedItem.multiplier
+//     console.log('count', upgradedItem.count)
+//     console.log('bubble increase', bubbleIncrease)
+
+
+//     console.log(bubbles)
+//   } else {
+
+//   }
+//   updateDraw()
+// }
+
+
+// function buyItem() {
+//   let purchaseItem = 
+// }
